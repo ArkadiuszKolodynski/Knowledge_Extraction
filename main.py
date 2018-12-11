916406673bb8e528a6de03fb7853461516375440
Arkadiusz Kołodyński <arekkolodynski@poczta.fm>
2018-12-11 01:00:58 +0100

    Poprawiony NER i zrobione parsowanie turtle'a (.ttl)

--- main.py
+++ main.py
@@ -1,34 +1,64 @@
 import rdflib
-import re
 import nltk
+from nltk.tag import StanfordNERTagger
+from nltk.chunk import conlltags2tree
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 
-REGEX = re.compile('[%s]' % re.escape('!@#$%^&*()_+=[]{}\|;:,.<>/?'))
-FILENAME = 'docs/2'
+FILENAME = 'request_data/file_1.ttl'
+CLASSIFIER_PATH = 'stanford_ner/english.all.3class.distsim.crf.ser.gz'
+NER_PATH = 'stanford_ner/stanford-ner.jar'
 
 
-def get_tokens(sentence):
-    # rozbicie zapytania na tokeny
+def stanford_ne_2_ibo(tagged_sent):
+    bio_tagged_sent = []
+    prev_tag = "O"
+    for token, tag in tagged_sent:
+        if tag == "O":  # O
+            bio_tagged_sent.append((token, tag))
+            prev_tag = tag
+            continue
+        if tag != "O" and prev_tag == "O":  # Begin NE
+            bio_tagged_sent.append((token, "B-"+tag))
+            prev_tag = tag
+        elif prev_tag != "O" and prev_tag == tag:  # Inside NE
+            bio_tagged_sent.append((token, "I-"+tag))
+            prev_tag = tag
+        elif prev_tag != "O" and prev_tag != tag:  # Adjacent NE
+            bio_tagged_sent.append((token, "B-"+tag))
+            prev_tag = tag
+
+    return bio_tagged_sent
+
+
+def stanford_ne_2_tree(ne_tagged_sent):
+    bio_tagged_sent = stanford_ne_2_ibo(ne_tagged_sent)
+    sent_tokens, sent_ne_tags = zip(*bio_tagged_sent)
+    sent_pos_tags = [pos for token, pos in nltk.pos_tag(sent_tokens)]
+
+    sent_conlltags = [(token, pos, ne) for token, pos, ne in zip(sent_tokens, sent_pos_tags, sent_ne_tags)]
+    ne_tree = conlltags2tree(sent_conlltags)
+    return ne_tree
+
+
+def get_entities(sentence):
     sentence = nltk.word_tokenize(sentence)
-    # usuniecie pozostalych znakow specjalnych
-    sentence = [REGEX.sub('', word) for word in sentence if REGEX.sub('', word) != '']
-    # part-of-speech tagger (interesuja nas tylko tagi NNP - 'noun, proper, singular')
-    query = nltk.pos_tag(sentence)
 
-    # MACHINE LEARNING do znajdowania entities w tekscie:
-    # https://www.commonlounge.com/discussion/2662a77ddcde4102a16d5eb6fa2eff1e
-    #
-    # ne_chunked_sents = nltk.ne_chunk(query)
-    # named_entities = []
-    # for tagged_tree in ne_chunked_sents:
-    #     if hasattr(tagged_tree, 'label'):
-    #         entity_name = ' '.join(c[0] for c in tagged_tree.leaves())
-    #         entity_type = tagged_tree.label()
-    #         named_entities.append((entity_name, entity_type))
-    # print(named_entities)
+    st = StanfordNERTagger(CLASSIFIER_PATH, NER_PATH, encoding='utf-8')
+    ne_tagged_sent = st.tag(sentence)
 
-    return [word[0] for word in query if word[1] == 'NNP']
+    ne_chunked_sent = stanford_ne_2_tree(ne_tagged_sent)
+    # print(ne_chunked_sent, '\n')
+
+    named_entities = []
+    for tagged_tree in ne_chunked_sent:
+        if hasattr(tagged_tree, 'label'):
+            entity_name = ' '.join(c[0] for c in tagged_tree.leaves())
+            entity_type = tagged_tree.label()
+            named_entities.append((entity_name, entity_type))
+    # print(named_entities, '\n')
+
+    return [entity[0] for entity in named_entities]
 
 
 def prepare_query(keyword):
@@ -81,35 +111,52 @@ def execute_query(keyword):
     return sparql.query().convert()
 
 
-def main():
-    sentence = open(FILENAME, 'r', encoding='utf=8').read()
-    print('\nDoc: ' + sentence + '\n')
+def get_request_string(graph):
+    predicate = rdflib.URIRef('http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#isString')
+    for s, p, o in graph:
+        if p == predicate:
+            return o
 
-    container = {}
+
+def get_entities_container(entities, sentence):
+    entity_container = {}
     offset = 0
-    tokens = get_tokens(sentence)
+    for entity in entities:
+        index = sentence.find(entity)
+        sentence = sentence.replace(entity, '', 1)
+        indexes_dict = {'beginIndex': index + offset, 'endIndex': index + offset + len(entity)}
+        if entity in entity_container:
+            entity_container[entity]['indexes'].append(indexes_dict)
+        else:
+            entity_container.update({entity: {'indexes': [indexes_dict]}})
+        offset += len(entity)
+    print('Found entities:', entities, '\n', entity_container, '\n')
+    return entity_container
 
-    if len(tokens):
-        for token in tokens:
-            index = sentence.find(token)
-            sentence = sentence.replace(token, '', 1)
-            indexes_dict = {'beginIndex': index + offset, 'endIndex': index + offset + len(token)}
-            if token in container:
-                container[token]['indexes'].append(indexes_dict)
-            else:
-                container.update({token: {'indexes': [indexes_dict]}})
-            offset += len(token)
-        print('Found tokens: ', tokens)
-        print(container)
-        print()
 
-        for token in container:
-            results = execute_query(token)
+def main():
+    g = rdflib.Graph()
+    g.parse(FILENAME, format='n3')
+
+    sentence = get_request_string(g)
+    print('\nSent: ' + sentence + '\n')
+
+    entities = get_entities(sentence)
+
+    if len(entities):
+        entity_container = get_entities_container(entities, sentence)
+
+        for entity in entity_container:
+            results = execute_query(entity)
+            if entity.endswith('s') and results['results']['bindings'] == []:
+                results = execute_query(entity[:-1])
             for result in results['results']['bindings']:
                 if result != {} and result['result']['value']:
-                    print(token, result['result']['value'])
+                    print(entity, result['result']['value'])
+
+        # https://rdflib.readthedocs.io/en/stable/intro_to_creating_rdf.html <= wynik wypisać do rdf'a
     else:
-        print('Tokens not found!')
+        print('No entities found!')
 
 
 if __name__ == '__main__':
